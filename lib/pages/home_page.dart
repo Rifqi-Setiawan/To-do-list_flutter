eb23976a2694cf544f076f7229dc6e844c5cca9c
Rifqi Setiawan <rifqis232@gmail.com>
2024-10-15 22:34:39 +0700

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_list_flutter/util/dialog_box.dart';
+import 'package:to_do_list_flutter/util/todo_tile.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List toDoList = [
+    ["Make tutorial", false],
+    ["Do Exercise", false],
+  ];
+
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
+  // text controller
+  final _controller = TextEditingController();
+
+  // save new task
+  void saveNewTask() {
+    setState(() {
+      toDoList.add([_controller.text, false]);
+      _controller.clear();
+    });
+        Navigator.of(context).pop();
+  }
+  void createNewTask() {
+    showDialog(context: context, builder: (context) {
+      return DialogBox(
+        controller: _controller,
+        onSave: saveNewTask,
+        onCancel: () => Navigator.of(context).pop(),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.yellow[200],
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("TO DO List - rifqi"),
+          elevation: 0,
+          backgroundColor: Colors.yellow,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: createNewTask,
+          child: Icon(Icons.add),
+          backgroundColor: Colors.yellow,
+          
+        ),
+        body: ListView.builder(
+          itemCount: toDoList.length,
+          itemBuilder: (context, index) {
+            return TodoTile(
+              taskName: toDoList[index][0],
+              taskCompleted: toDoList[index][1],
+              onChanged: (value) => checkBoxChanged(value, index),
+            );
+          },
+        ));
+  }
+}
---
+++ lib/util/todo_tile.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+
+class TodoTile extends StatelessWidget {
+  final String taskName;
+  final bool taskCompleted;
+  Function (bool?) ? onChanged;
+
+  TodoTile({
+    super.key, 
+    required this.taskName, 
+    required this.taskCompleted,
+    required this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 25, right: 25, top: 25),
+      child: Slidable(
+        endActionPane: StretchMotion(
+          
+        ),
+        child: Container(
+          padding: EdgeInsets.all(24),
+          child: Row(
+            children: [
+              // check box
+               Checkbox(value: taskCompleted, onChanged: onChanged),
+        
+              // task name
+              Text(taskName, 
+              style: TextStyle(decoration: taskCompleted ? TextDecoration.lineThrough : TextDecoration.none),),
+            ],
+          ),
+          decoration: BoxDecoration(
+            color: Colors.yellow,
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+      ),
+    );
+  }
+}
